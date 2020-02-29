2e3a5ba0a11a2f5a4219be7187cc5c22d8a411a5
Jaemin Seo <86sjmjm@gmail.com>
2020-03-01 01:42:44 +0900

    Updates

---
+++ precache-manifest.44839e86f9aa368b755e6dc43996ac18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b0ce598b6d264b5e408018cbf713171",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e8ef4a495c6f85a2348c",
+    "url": "/movie_app_2019/static/css/main.276c81d6.chunk.css"
+  },
+  {
+    "revision": "1558012c2b3e9d434035",
+    "url": "/movie_app_2019/static/js/2.5537d7ad.chunk.js"
+  },
+  {
+    "revision": "970d7393064dc8df8708ab396e4ac92c",
+    "url": "/movie_app_2019/static/js/2.5537d7ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8ef4a495c6f85a2348c",
+    "url": "/movie_app_2019/static/js/main.a5f05b87.chunk.js"
+  },
+  {
+    "revision": "0f87585caaeceef6a69a",
+    "url": "/movie_app_2019/static/js/runtime-main.da932103.js"
+  }
+]);
